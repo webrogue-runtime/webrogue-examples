df4b4381454448bb8b4fdca897b1ac9ff5f348a5
Artem Borovik <polzovatellllk@gmail.com>
2024-08-30 21:35:35 +0300

    raylib fixes

--- raylib/main.c
+++ raylib/main.c
@@ -1,121 +1,167 @@
 /*******************************************************************************************
- *
- *   raylib [models] example - Waving cubes
- *
- *   Example originally created with raylib 2.5, last time updated with
- *raylib 3.7
- *
- *   Example contributed by Codecat (@codecat) and reviewed by Ramon Santamaria
- *(@raysan5)
- *
- *   Example licensed under an unmodified zlib/libpng license, which is an
- *OSI-certified, BSD-like license that allows static linking with closed source
- *software
- *
- *   Copyright (c) 2019-2024 Codecat (@codecat) and Ramon Santamaria (@raysan5)
- *
- ********************************************************************************************/
+*
+*   raylib [core] example - 2d camera split screen
+*
+*   Addapted from the core_3d_camera_split_screen example: 
+*       https://github.com/raysan5/raylib/blob/master/examples/core/core_3d_camera_split_screen.c
+*
+*   Example originally created with raylib 4.5, last time updated with raylib 4.5
+*
+*   Example contributed by Gabriel dos Santos Sanches (@gabrielssanches) and reviewed by Ramon Santamaria (@raysan5)
+*
+*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
+*   BSD-like license that allows static linking with closed source software
+*
+*   Copyright (c) 2023 Gabriel dos Santos Sanches (@gabrielssanches)
+*
+********************************************************************************************/
 
 #include "raylib.h"
 
-#include <math.h> // Required for: sinf()
+#define PLAYER_SIZE 40
 
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
-int main() {
-  // Initialization
-  //--------------------------------------------------------------------------------------
-  const int screenWidth = 800;
-  const int screenHeight = 450;
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 440;
 
-  InitWindow(screenWidth, screenHeight,
-             "raylib [models] example - waving cubes");
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - 2d camera split screen");
 
-  // Initialize the camera
-  Camera3D camera = {0};
-  camera.position = (Vector3){30.0f, 20.0f, 30.0f}; // Camera position
-  camera.target = (Vector3){0.0f, 0.0f, 0.0f};      // Camera looking at point
-  camera.up =
-      (Vector3){0.0f, 1.0f, 0.0f}; // Camera up vector (rotation towards target)
-  camera.fovy = 70.0f;             // Camera field-of-view Y
-  camera.projection = CAMERA_PERSPECTIVE; // Camera projection type
+    Rectangle player1 = { 200, 200, PLAYER_SIZE, PLAYER_SIZE };
+    Rectangle player2 = { 250, 200, PLAYER_SIZE, PLAYER_SIZE };
 
-  // Specify the amount of blocks in each direction
-  const int numBlocks = 15;
+    Camera2D camera1 = { 0 };
+    camera1.target = (Vector2){ player1.x, player1.y };
+    camera1.offset = (Vector2){ 200.0f, 200.0f };
+    camera1.rotation = 0.0f;
+    camera1.zoom = 1.0f;
 
-  SetTargetFPS(60);
-  //--------------------------------------------------------------------------------------
+    Camera2D camera2 = { 0 };
+    camera2.target = (Vector2){ player2.x, player2.y };
+    camera2.offset = (Vector2){ 200.0f, 200.0f };
+    camera2.rotation = 0.0f;
+    camera2.zoom = 1.0f;
 
-  // Main game loop
-  while (!WindowShouldClose()) // Detect window close button or ESC key
-  {
-    // Update
-    //----------------------------------------------------------------------------------
-    double time = GetTime();
+    RenderTexture screenCamera1 = LoadRenderTexture(screenWidth/2, screenHeight);
+    RenderTexture screenCamera2 = LoadRenderTexture(screenWidth/2, screenHeight);
 
-    // Calculate time scale for cube position and size
-    float scale = (2.0f + (float)sin(time)) * 0.7f;
+    // Build a flipped rectangle the size of the split view to use for drawing later
+    Rectangle splitScreenRect = { 0.0f, 0.0f, (float)screenCamera1.texture.width, (float)-screenCamera1.texture.height };
 
-    // Move camera around the scene
-    double cameraTime = time * 0.3;
-    camera.position.x = (float)cos(cameraTime) * 40.0f;
-    camera.position.z = (float)sin(cameraTime) * 40.0f;
-    //----------------------------------------------------------------------------------
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
 
-    // Draw
-    //----------------------------------------------------------------------------------
-    BeginDrawing();
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        if (IsKeyDown(KEY_S)) player1.y += 3.0f;
+        else if (IsKeyDown(KEY_W)) player1.y -= 3.0f;
+        if (IsKeyDown(KEY_D)) player1.x += 3.0f;
+        else if (IsKeyDown(KEY_A)) player1.x -= 3.0f;
 
-    ClearBackground(RAYWHITE);
+        if (IsKeyDown(KEY_UP)) player2.y -= 3.0f;
+        else if (IsKeyDown(KEY_DOWN)) player2.y += 3.0f;
+        if (IsKeyDown(KEY_RIGHT)) player2.x += 3.0f;
+        else if (IsKeyDown(KEY_LEFT)) player2.x -= 3.0f;
 
-    BeginMode3D(camera);
+        camera1.target = (Vector2){ player1.x, player1.y };
+        camera2.target = (Vector2){ player2.x, player2.y };
+        //----------------------------------------------------------------------------------
 
-    DrawGrid(10, 5.0f);
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginTextureMode(screenCamera1);
+            ClearBackground(RAYWHITE);
+            
+            BeginMode2D(camera1);
+            
+                // Draw full scene with first camera
+                for (int i = 0; i < screenWidth/PLAYER_SIZE + 1; i++)
+                {
+                    DrawLineV((Vector2){(float)PLAYER_SIZE*i, 0}, (Vector2){ (float)PLAYER_SIZE*i, (float)screenHeight}, LIGHTGRAY);
+                }
 
-    for (int x = 0; x < numBlocks; x++) {
-      for (int y = 0; y < numBlocks; y++) {
-        for (int z = 0; z < numBlocks; z++) {
-          // Scale of the blocks depends on x/y/z positions
-          float blockScale = (x + y + z) / 30.0f;
+                for (int i = 0; i < screenHeight/PLAYER_SIZE + 1; i++)
+                {
+                    DrawLineV((Vector2){0, (float)PLAYER_SIZE*i}, (Vector2){ (float)screenWidth, (float)PLAYER_SIZE*i}, LIGHTGRAY);
+                }
 
-          // Scatter makes the waving effect by adding blockScale over time
-          float scatter = sinf(blockScale * 20.0f + (float)(time * 4.0f));
+                for (int i = 0; i < screenWidth/PLAYER_SIZE; i++)
+                {
+                    for (int j = 0; j < screenHeight/PLAYER_SIZE; j++)
+                    {
+                        DrawText(TextFormat("[%i,%i]", i, j), 10 + PLAYER_SIZE*i, 15 + PLAYER_SIZE*j, 10, LIGHTGRAY);
+                    }
+                }
 
-          // Calculate the cube position
-          Vector3 cubePos = {
-              (float)(x - numBlocks / 2) * (scale * 3.0f) + scatter,
-              (float)(y - numBlocks / 2) * (scale * 2.0f) + scatter,
-              (float)(z - numBlocks / 2) * (scale * 3.0f) + scatter};
+                DrawRectangleRec(player1, RED);
+                DrawRectangleRec(player2, BLUE);
+            EndMode2D();
+            
+            DrawRectangle(0, 0, GetScreenWidth()/2, 30, Fade(RAYWHITE, 0.6f));
+            DrawText("PLAYER1: W/S/A/D to move", 10, 10, 10, MAROON);
+            
+        EndTextureMode();
 
-          // Pick a color with a hue depending on cube position for the rainbow
-          // color effect NOTE: This function is quite costly to be done per
-          // cube and frame, pre-catching the results into a separate array
-          // could improve performance
-          Color cubeColor =
-              ColorFromHSV((float)(((x + y + z) * 18) % 360), 0.75f, 0.9f);
+        BeginTextureMode(screenCamera2);
+            ClearBackground(RAYWHITE);
+            
+            BeginMode2D(camera2);
+            
+                // Draw full scene with second camera
+                for (int i = 0; i < screenWidth/PLAYER_SIZE + 1; i++)
+                {
+                    DrawLineV((Vector2){ (float)PLAYER_SIZE*i, 0}, (Vector2){ (float)PLAYER_SIZE*i, (float)screenHeight}, LIGHTGRAY);
+                }
 
-          // Calculate cube size
-          float cubeSize = (2.4f - scale) * blockScale;
+                for (int i = 0; i < screenHeight/PLAYER_SIZE + 1; i++)
+                {
+                    DrawLineV((Vector2){0, (float)PLAYER_SIZE*i}, (Vector2){ (float)screenWidth, (float)PLAYER_SIZE*i}, LIGHTGRAY);
+                }
 
-          // And finally, draw the cube!
-          DrawCube(cubePos, cubeSize, cubeSize, cubeSize, cubeColor);
-        }
-      }
+                for (int i = 0; i < screenWidth/PLAYER_SIZE; i++)
+                {
+                    for (int j = 0; j < screenHeight/PLAYER_SIZE; j++)
+                    {
+                        DrawText(TextFormat("[%i,%i]", i, j), 10 + PLAYER_SIZE*i, 15 + PLAYER_SIZE*j, 10, LIGHTGRAY);
+                    }
+                }
+
+                DrawRectangleRec(player1, RED);
+                DrawRectangleRec(player2, BLUE);
+                
+            EndMode2D();
+            
+            DrawRectangle(0, 0, GetScreenWidth()/2, 30, Fade(RAYWHITE, 0.6f));
+            DrawText("PLAYER2: UP/DOWN/LEFT/RIGHT to move", 10, 10, 10, DARKBLUE);
+            
+        EndTextureMode();
+
+        // Draw both views render textures to the screen side by side
+        BeginDrawing();
+            ClearBackground(BLACK);
+            
+            DrawTextureRec(screenCamera1.texture, splitScreenRect, (Vector2){ 0, 0 }, WHITE);
+            DrawTextureRec(screenCamera2.texture, splitScreenRect, (Vector2){ screenWidth/2.0f, 0 }, WHITE);
+            
+            DrawRectangle(GetScreenWidth()/2 - 2, 0, 4, GetScreenHeight(), LIGHTGRAY);
+        EndDrawing();
     }
 
-    EndMode3D();
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    UnloadRenderTexture(screenCamera1); // Unload render texture
+    UnloadRenderTexture(screenCamera2); // Unload render texture
 
-    DrawFPS(10, 10);
+    CloseWindow();                      // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
 
-    EndDrawing();
-    //----------------------------------------------------------------------------------
-  }
-
-  // De-Initialization
-  //--------------------------------------------------------------------------------------
-  CloseWindow(); // Close window and OpenGL context
-  //--------------------------------------------------------------------------------------
-
-  return 0;
+    return 0;
 }
